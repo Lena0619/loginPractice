26088296a876a3ebacdf671ab7f110b083b8b312
Lena Lin <lenalin9977@gmail.com>
2020-11-12 21:21:59 +0800

    將header和footer各自移入獨立的檔案.新增註冊帳號及忘記密碼的檔案.

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>Document</title>
-    <style>
-    body {
-        background: #ccc;
-    }
-    </style>
-</head>
-<body>
+<?php
+include_once "header.php";
+?>
+
 
 <div class="container w-50 border p-5 mt-5 bg-dark text-light rounded-lg">
 <form>
@@ -23,17 +13,15 @@
     <label for="exampleInputPassword1">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
-  <div class="form-group">
-     <a href="" class="pr-5">忘記密碼?</a>
-     <a href="" class="pr-5">註冊新帳號.</a>
+  <div class="form-group d-flex justify-content-end">
+     <a href="forget.php" class="pr-3">忘記密碼?</a>
+     <a href="register.php" class="">註冊新帳號.</a>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </div>
 
 
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> 
-</body>
-</html>
+<?php
+include_once "footer.php";
+?>
